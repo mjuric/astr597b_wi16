788902be29f9fc3b69218020e7c6f15ee68040b0
Mario Juric <mjuric@astro.washington.edu>
2016-02-09 11:12:16 -0800

    Added materials for lecture 11 ("Hands-on with LSD, part 2")

---
+++ lectures/2016-02-09-lsd-handson-part2/setup.sh
@@ -0,0 +1,5 @@
+export PATH="/astro/apps6/opt/anaconda2.4/bin:$PATH"
+export LSD_CACHEDIR=$HOME/tempdir
+export LSD_DB=$HOME/lsddb:/astro/apps6/opt/lsddb
+export LSD_TEMPDIR=$HOME/tempdir
+export NWORKERS=8
